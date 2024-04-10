e9c0afa01e979c75d5d9510cac8e674bf1148286
TUPKAP <hexxacque@yahoo.it>
2024-04-10 10:32:42 +0200

    blade component alpine-multiselect

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,16 +1,20 @@
 <div>
+
     <div x-data="{ count: 0 }">
+
         <button @click="count++">Add</button>
         <span x-text="count">0</span>
+
     </div>
 
-    <div class="w-full" x-data="alpineMuliSelect({selected:['te_11', 'te_12'], elementId:'multSelect'})">
+    <x-alpine-multiselect preselect-values="'te_1', 'te_3'"
+                          id-select="multSelect"
+                          name="test"
+    >
+        <option value="te_1" data-search="arsenal">Arsenal</option>
+        <option value="te_2" data-search="Tottenham Hotspur Spurs">Spurs</option>
+        <option value="te_3" data-search="Manchester City">Man City</option>
 
-            <select style="display:none;" id="multSelect">
-                <option value="te_1" data-search="arsenal">Arsenal</option>
-                <option value="te_3" data-search="Tottenham Hotspur Spurs">Spurs</option>
-                <option value="te_3" data-search="Manchester City">Man City</option>
-            </select>
-        </div>
+    </x-alpine-multiselect>
 
 </div>
